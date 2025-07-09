7c707ff01d9a49b4942eb113b6f2280d8d53e889
eduardo <eduardo021cruz@gmail.com>
2025-07-09 14:25:21 -0300

    rotas e models

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection')
+const express    = require('express');
+const app        = express();
+const db         = require('./db/connection')
+const bodyParser = require('body-parser')
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O express esta rodando na porta ${PORT}`);
 });
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //db connetion
 db
@@ -22,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.end("Está funcionando!");
 });
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
